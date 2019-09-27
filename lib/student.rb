f2897a6201b069b64e137da6404fe58eb591af25
github username <github email address>
2019-09-26 22:42:09 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,3 @@
-require 'pry'
 class Student
   attr_accessor :name, :grade 
   attr_reader :id
@@ -14,10 +13,8 @@ class Student
       INSERT INTO students (name, grade) VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    binding.pry
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
-
 
 
   def self.create_table
